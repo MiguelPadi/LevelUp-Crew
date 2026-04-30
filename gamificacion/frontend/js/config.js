35a40cde1331c0b5faa427a3c82d88647088f720
MiguelPadi <markrey088@gmail.com>
2026-04-29 21:43:46 -0400

    URL

--- gamificacion/frontend/js/config.js
+++ gamificacion/frontend/js/config.js
@@ -5,7 +5,7 @@ const ENV = {
     API_URL: "http://localhost:5000/api",
   },
   production: {
-    API_URL: "http://3.134.77.54:5000/api",
+    API_URL: "http://levelup-crew.duckdns.org:5000/api",
     // Cuando tengas dominio:
     // API_URL: "https://tu-dominio.com/api",
   }
